b571885d685a5afe449a246861f4179fee9a1c68
Allani1010 <allan.dc@hotmail.fr>
2022-12-07 15:25:33 +0100

    add music

---
+++ src/VueControleur/Music.java
@@ -0,0 +1,39 @@
+package VueControleur;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+import java.io.IOException;
+
+public class Music {
+    Clip clip;
+    AudioInputStream sound;
+
+    public Music(String soundFileName) {
+        setFile(soundFileName);
+    }
+
+    public void setFile(String soundFileName) {
+        try {
+            File file = new File(soundFileName);
+            sound = AudioSystem.getAudioInputStream(file);
+            clip = AudioSystem.getClip();
+            clip.open(sound);
+        } catch (Exception e) {
+            System.out.println("Error initialisation music");
+        }
+    }
+    public void play() {
+        try {
+            clip.start();
+        } catch (Exception e) {
+            System.out.println("Error lecture music");
+        }
+    }
+    public void stop() throws IOException {
+        sound.close();
+        clip.close();
+        clip.stop();
+    }
+}
